ae1600fe7d5b65659253b53d9bbf53fea33a5791
giuseppebaldini <giuseppebaldini@live.com>
2022-04-19 17:12:27 +0100

    Add 1.2: refactor Content component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,23 +1,33 @@
-const Header = ({course}) => {
+const Header = (props) => {
   return (
     <>
-      <h1>{course}</h1>
+      <h1>{props.course}</h1>
     </>
   )
 }
 
-const Content = ({part, exercises}) => {
+const Part = (props) => {
   return (
     <>
-      <p>{part} {exercises}</p>
+      <p>{props.part} {props.exercises}</p>
     </>
   )
 }
 
-const Total = ({total}) => {
+const Content = (props) => {
   return (
     <>
-      <p>Number of exercises {total}</p>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
     </>
   )
 }
@@ -34,9 +44,9 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} 
+               part2={part2} exercises2={exercises2} 
+               part3={part3} exercises3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
